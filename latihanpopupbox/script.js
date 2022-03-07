d625d51cbe5f18da5b3016f7733c19c6cb3afa8c
meidaandini <99390692+meidaandini@users.noreply.github.com>
2022-03-07 10:30:19 +0700

    popup box javascript video ke-16

---
+++ latihanpopupbox/script.js
@@ -0,0 +1,19 @@
+// popup box : alert
+var tanya = alert('Selamat datang');
+
+// popup box : prompt
+
+var email = prompt('masukkan nama:');
+prompt('masukkan email:');
+prompt('masukkan pasword!:');
+// popup box : confirm
+alert('pasword salah');
+confirm('coba lagi?');
+prompt('masukkan email:');
+prompt('masukkan pasword!:');
+confirm('simpan password?');
+if (tes === true) {
+    alert('email tersimpan');
+} else {
+    alert('tidak tersimpan');
+}
