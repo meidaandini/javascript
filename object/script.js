064ed5c299efdde3edd22104610895633622d6a1
meidaandini <99390692+meidaandini@users.noreply.github.com>
2022-03-12 14:35:01 +0700

    Add files via upload
    materi object video ke 41

---
+++ object/script.js
@@ -0,0 +1,11 @@
+// membuat object
+var mhs = {
+	nama : "Meida Andini",
+	umur : 31,
+	ips : [3.00, 2.50, 3.20,],
+	alamat : {
+		jalan : "Jl. ringin No. 08",
+		kota : "Bandung",
+		provinsi : "Jawa Barat"
+	}
+};
